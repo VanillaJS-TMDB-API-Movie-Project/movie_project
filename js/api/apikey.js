7bb656fecda8b14dd4a44ac4f588e8f7467002be
이준구 <leejungoo1396@gmail.com>
2024-01-11 18:41:15 +0900

    test4

---
+++ js/api/apikey.js
@@ -0,0 +1,5 @@
+const config = {
+  APIKey:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzJkOGRjYWYwNTk1N2I0Y2MwNzgzYmQ5MzAyMzc4MiIsInN1YiI6IjY1OTY0OGE2NTkwN2RlMDk1MjYzYmYxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YqIDxHAXnnf_hBndtoVq8hT90OnxLTszv9onSkZdVRM"
+};
+export { config };
