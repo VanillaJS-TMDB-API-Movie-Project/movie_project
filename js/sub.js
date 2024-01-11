3ef9a6c07836dd3cabb1cafed41bad071ba5f767
이준구 <leejungoo1396@gmail.com>
2024-01-11 12:09:27 +0900

    test

---
+++ js/sub.js
@@ -0,0 +1,15 @@
+let data = {
+  a: "/Users/t2023-m0063/Downloads/oMTuRG8koeKqH2tVonaWhyp7OBw.jpg",
+  b: 20
+};
+
+
+
+let movies_content = document.getSelection("movies-contetnt");
+
+movies_content.addEventListener("click" {
+
+
+
+})
+
