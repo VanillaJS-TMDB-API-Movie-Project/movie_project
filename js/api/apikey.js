bccbf1cf65e2eea42a3c6241cdca9e22266f3d80
baram5 <asd7973@gmail.com>
2024-01-16 00:09:26 +0900

    api 키 수정

--- js/api/apikey.js
+++ js/api/apikey.js
@@ -1,11 +1,1 @@
 export const tmdbApiKey = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzJkOGRjYWYwNTk1N2I0Y2MwNzgzYmQ5MzAyMzc4MiIsInN1YiI6IjY1OTY0OGE2NTkwN2RlMDk1MjYzYmYxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YqIDxHAXnnf_hBndtoVq8hT90OnxLTszv9onSkZdVRM';
-
-export const firebaseConfig = {
-    apiKey: "AIzaSyDk_ezxm_ONBld8BzJr0bOJQPF8D_TwETM",
-    authDomain: "sparta-161b0.firebaseapp.com",
-    projectId: "sparta-161b0",
-    storageBucket: "sparta-161b0.appspot.com",
-    messagingSenderId: "744285920872",
-    appId: "1:744285920872:web:6ec585c016f47168d7a5dd",
-    measurementId: "G-P4HWMGFH7J"
-};
